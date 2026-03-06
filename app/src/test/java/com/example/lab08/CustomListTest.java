348789b39e5bc79d1a17912056659fd483e9a287
Varad P <varad@ualberta.ca>
2026-03-06 16:31:26 -0700

    Red: add failing test for hasCity()

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -2,6 +2,25 @@ package com.example.lab08;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
+/**
+   Testing hasCity, delete and countCities methods using TDD
+ */
 public class CustomListTest {
 
+    /**
+     * Test hasCity method which checks if a city exists in the list
+     * RED phase- this test will fail because hasCity() doesn't exist yet
+     */
+    @Test
+    public void testHasCity() {
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+
+        list.addCity(calgary);
+        //assertTrue(list.hasCity(calgary), "Should return true for city in list");
+
+        City edmonton = new City("Edmonton", "AB");
+        //assertFalse(list.hasCity(edmonton), "Should return false for city not in list");
+    }
 }
